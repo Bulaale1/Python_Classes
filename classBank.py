a619ccc366916cac8aad699b6289f598da781155
Yasin Hussein <bulaale116@gmail.com>
2026-03-10 02:50:26 -0600

    implementation of bank_account, Circle, Rectangle, and Vehicle parent class with sub classes

--- classBank.py
+++ classBank.py
@@ -1,4 +1,5 @@
 import math
+from datetime import date
 print('--- Welcome To Python Classes ---')
 print('1:Bank acount class: ')
 print('2:Circle class: ')
@@ -12,14 +13,19 @@ class Bank_account:
         self.transaction_history = []
 
     def deposit(self, amount):
-        if amount <= 0:
-            print(f"❌ Error: Cannot deposit {amount}. Amount must be positive.")
-            return
         
-        self.balance += amount
-        # now = datetime.datetime.now()
-        self.transaction_history.append(f"+{amount}")
-        print(f"✅ Deposited ${amount}. New balance: ${self.balance} ")
+        try:
+            amount = float(amount)
+            if amount <= 0:
+                print(f"❌ Error: Cannot deposit {amount}. Amount must be positive.")
+                return
+            
+            self.balance += amount
+            # now = datetime.datetime.now()
+            self.transaction_history.append(f"+{amount}")
+            print(f"✅ Deposited ${amount}. New balance: ${self.balance} ")
+        except ValueError:
+            print("❌ Error: Please enter a valid numerical amount.")
 
     def withdraw(self, amount):
         if amount <= 0:
@@ -42,19 +48,20 @@ class Bank_account:
         for entry in self.transaction_history:
             status = 'Deposit' if entry.startswith('+') else 'Withdrawal'
             print(f' {status:12}: {entry}')
-
+# --- Execution ---
 #   Class BankAccount instance
 account_1 = Bank_account('Bulaale116',580)
 account_1.deposit(250)
 account_1.deposit(350)
 account_1.deposit(-250)
+account_1.deposit('million')
 account_1.withdraw(57)
 account_1.withdraw(0)
 account_1.details()
 account_1.statement()
-print('*'*100)
+
 print('---End of bank account class---')
-print('*'*100)
+print("-" * 20)
 
 # 2. Circle class.
 # Declare a circle class
@@ -77,7 +84,7 @@ class Circle:
     def __str__(self):
         return f"Circle class with :{self.radius} radius"
     
-
+# --- Execution ---
 #   Circle class intance.
 circle = Circle(25)
 print('Before resizing the radius')
@@ -86,7 +93,7 @@ print(circle)
 print(f"The area of circle : {circle.area():.2f}")
 print(f"The circumference of circle is :{circle.circumference():.2f}")
 
-print('*'*100)
+print("-" * 20)
 
 print('After resizing the radius') 
 circle.resize(50)
@@ -96,7 +103,7 @@ print(f"The area of circle : {circle.area():.2f}")
 print(f"The circumference of circle is :{circle.circumference():.2f}")
 
 print('---End of Circle class---')
-print('*'*100)
+print("-" * 20)
 
 
 #   3. Rectangle class .
@@ -110,13 +117,76 @@ class Rectangle:
         return self.width * self.length
     # calculate the perimeter of rectangle and return to it
     def perimeter (self):
-        return 2*self.length*self.width
+        return 2*(self.length+self.width)
     def __str__(self):
         return f"Class Rectangle with length and width of :{self.length,self.width}"
+# --- Execution ---
 #   Rectangle class instance
 rectangle = Rectangle(10,5)
 print(rectangle)
 print(f"The area of rectangle with {rectangle.length} and width of {rectangle.width} is: {rectangle.area()}")
 print(f"The Perimeter of rectangle with {rectangle.length} and width of {rectangle.width} is: {rectangle.perimeter()}")
 print('---End of Rectangle class---')
-print('*'*100)
+print("-" * 20)
+
+
+
+# 4.    Inheritance 
+class Vehicle:
+    def __init__(self,make, model, year):
+        self.make = make
+        self.model = model
+        self.year = int(year)
+    def get_resale_value(self):
+        value = 20000 - (date.today().year - self.year) * 1500
+        return max(0,value)
+    def get_age(self):
+        return date.today().year  - self.year
+    def describe(self):
+        return f"This vehicle is {self.make}, {self.model}, {self.year}"
+    def __str__(self):
+        return f"This is Vehicle class with make:{self.make},model:{self.model},and manufactured on :{self.year}"
+class Car(Vehicle):
+    def __init__(self, make, model, year,number_of_doors):
+        super().__init__(make, model, year)
+        self.number_of_doors = number_of_doors
+    def describe(self):
+        return f"{super().describe()} with {self.number_of_doors} doors"
+    
+# Child Class 2: Motorcycle, which adds an attribute for has_sidecar.
+
+class Motorcycle(Vehicle):
+    def __init__(self, make, model, year,has_sidecar = False):
+        super().__init__(make,model, year)
+        self.has_sidecar = has_sidecar
+    def describe (self):
+        suffix = 'with sidecar' if self.has_sidecar else 'without sidecar'
+        return f"{super().describe()} {suffix}"
+    
+
+# --- Execution ---
+car = Car('Tesla', 'Model 3', 2023, 4)
+motorcycle = Motorcycle('Yamaha', 'MT-07', 2011, False)
+motorcycle1 = Motorcycle('Toyota', 'MT-09', 2014, True)
+motorcycle2 = Motorcycle('Yamaha', 'MT-05', 2009)
+
+print(car.describe())
+print(f"Resale Value: $CAD {car.get_resale_value()}")
+print(f"Age: {car.get_age()} years")
+
+print("-" * 20)
+
+print(motorcycle.describe())
+print(f"Resale Value: $CAD {motorcycle.get_resale_value()}")
+print(f"Age: {motorcycle.get_age()} years") 
+print("-" * 20)
+
+print(motorcycle1.describe())
+print(f"Resale Value: $CAD {motorcycle1.get_resale_value()}")
+print(f"Age: {motorcycle1.get_age()} years") 
+
+print("-" * 20)
+
+print(motorcycle2.describe())
+print(f"Resale Value: $CAD {motorcycle2.get_resale_value()}")
+print(f"Age: {motorcycle2.get_age()} years") 
